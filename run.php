77ebe56ad16dc17dd4358803c67362e5be24ef79
Nicolas Terray <nicolas.terray@enalean.com>
2019-02-27 18:26:26 +0100

    Deactivate Xdebug
    Because it is not nice with PHP-Parser

--- run.php
+++ run.php
@@ -6,32 +6,38 @@
 
 require_once 'vendor/autoload.php';
 
+use Composer\XdebugHandler\XdebugHandler;
 use Monolog\Logger;
 use Monolog\Handler\ErrorLogHandler;
+use Tab2Gettext\Tab2Gettext;
+
+$xdebug = new XdebugHandler('tab2gettext');
+$xdebug->check();
+unset($xdebug);
 
 $log = new Logger('log');
 $log->pushHandler(new ErrorLogHandler());
 
 try {
-    $reflector = new \Tab2Gettext\Tab2Gettext($log);
+    $reflector = new Tab2Gettext($log);
 
     if (! isset($argv[1]) && ! is_dir($argv[1])) {
-        throw new \RuntimeException("Please provide a directory as first parameter");
+        throw new RuntimeException("Please provide a directory as first parameter");
     }
     $filepath = $argv[1];
 
     if (! isset($argv[2])) {
-        throw new \RuntimeException("Please provide a primary key as second parameter");
+        throw new RuntimeException("Please provide a primary key as second parameter");
     }
     $primarykey = $argv[2];
 
     if (! isset($argv[3])) {
-        throw new \RuntimeException("Please provide a domain as third parameter");
+        throw new RuntimeException("Please provide a domain as third parameter");
     }
     $domain = $argv[3];
 
     if (! isset($argv[4]) && ! is_file($argv[4])) {
-        throw new \RuntimeException("Please provide a cache lang path as fourth parameter");
+        throw new RuntimeException("Please provide a cache lang path as fourth parameter");
     }
     $cachelangpath = $argv[4];
 
